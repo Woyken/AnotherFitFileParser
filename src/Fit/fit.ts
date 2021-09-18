37a836a415c5c96c7120aa4b82dea9a85d82c8d6
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-09-18 13:22:26 +0300

    cleanup

--- src/Fit/fit.ts
+++ src/Fit/fit.ts
@@ -36,46 +36,46 @@ export class FitType {
 
 // tslint:disable-next-line: max-classes-per-file
 export class Fit {
-    public static readonly protocolVersionMajorShift: number = 4;
+    public static readonly protocolVersionMajorShift = 4;
     // tslint:disable-next-line: max-line-length
-    public static readonly protocolVersionMajorMask: number = (0x0F << Fit.protocolVersionMajorShift);
+    public static readonly protocolVersionMajorMask = (0x0F << Fit.protocolVersionMajorShift);
 
     public static readonly protocolVersion: number = ProtocolVersion.V20.version;
     public static readonly protocolMajorVersion: number = ProtocolVersion.V20.majorVersion;
     public static readonly protocolMinorVersion: number = ProtocolVersion.V20.minorVersion;
 
-    public static readonly profileMajorVersion: number = 20;
-    public static readonly profileMinorVersion: number = 96;
+    public static readonly profileMajorVersion = 20;
+    public static readonly profileMinorVersion = 96;
     // tslint:disable-next-line: max-line-length
-    public static readonly profileVersion: number = ((Fit.profileMajorVersion * 100) + Fit.profileMinorVersion);
+    public static readonly profileVersion = ((Fit.profileMajorVersion * 100) + Fit.profileMinorVersion);
 
-    public static readonly headerTypeMask: number = 0xF0;
-    public static readonly compressedHeaderMask: number = 0x80;
-    public static readonly compressedTimeMask: number = 0x1F;
-    public static readonly compressedLocalMesgNumMask: number = 0x60;
+    public static readonly headerTypeMask = 0xF0;
+    public static readonly compressedHeaderMask = 0x80;
+    public static readonly compressedTimeMask = 0x1F;
+    public static readonly compressedLocalMesgNumMask = 0x60;
 
-    public static readonly mesgDefinitionMask: number = 0x40;
-    public static readonly devDataMask: number = 0x20;
-    public static readonly mesgHeaderMask: number = 0x00;
-    public static readonly localMesgNumMask: number = 0x0F;
-    public static readonly maxLocalMesgs: number = Fit.localMesgNumMask + 1;
+    public static readonly mesgDefinitionMask = 0x40;
+    public static readonly devDataMask = 0x20;
+    public static readonly mesgHeaderMask = 0x00;
+    public static readonly localMesgNumMask = 0x0F;
+    public static readonly maxLocalMesgs = Fit.localMesgNumMask + 1;
 
-    public static readonly mesgDefinitionReserved: number = 0x00;
-    public static readonly littleEndian: number = 0x00;
-    public static readonly bigEndian: number = 0x01;
+    public static readonly mesgDefinitionReserved = 0x00;
+    public static readonly littleEndian = 0x00;
+    public static readonly bigEndian = 0x01;
 
-    public static readonly maxMesgSize: number = 255;
-    public static readonly maxFieldSize: number = 255;
+    public static readonly maxMesgSize = 255;
+    public static readonly maxFieldSize = 255;
 
-    public static readonly headerWithCRCSize: number = 14;
-    public static readonly headerWithoutCRCSize: number = (Fit.headerWithCRCSize - 2);
+    public static readonly headerWithCRCSize = 14;
+    public static readonly headerWithoutCRCSize = (Fit.headerWithCRCSize - 2);
 
-    public static readonly fieldNumInvalid: number = 255;
-    public static readonly fieldNumTimeStamp: number = 253;
+    public static readonly fieldNumInvalid = 255;
+    public static readonly fieldNumTimeStamp = 253;
 
-    public static readonly subfieldIndexActiveSubfield: number = 0xFFFE;
-    public static readonly subfieldIndexMainField: number = Fit.subfieldIndexActiveSubfield + 1;
-    public static readonly subfieldNameMainField: string = '';
+    public static readonly subfieldIndexActiveSubfield = 0xFFFE;
+    public static readonly subfieldIndexMainField = Fit.subfieldIndexActiveSubfield + 1;
+    public static readonly subfieldNameMainField = '';
 
     public static baseType: FitType[] = [
         new FitType(false, 0x00, 'enum', 0xFF, 1, false, false),
@@ -99,24 +99,24 @@ export class Fit {
         new FitType(true, 0x90, 'uint64z', 0x0000000000000000, 8, false, true)];
 
     // Index into the BaseTypes array
-    public static readonly enum: number = 0x00;
-    public static readonly sInt8: number = 0x01;
-    public static readonly uInt8: number = 0x02;
-    public static readonly sInt16: number = 0x03;
-    public static readonly uInt16: number = 0x04;
-    public static readonly sInt32: number = 0x05;
-    public static readonly uInt32: number = 0x06;
-    public static readonly string: number = 0x07;
-    public static readonly float32: number = 0x08;
-    public static readonly float64: number = 0x09;
-    public static readonly uInt8z: number = 0x0A;
-    public static readonly uInt16z: number = 0x0B;
-    public static readonly uInt32z: number = 0x0C;
-    public static readonly byte: number = 0x0D;
-    public static readonly sInt64: number = 0x0E;
-    public static readonly uInt64: number = 0x0F;
-    public static readonly uInt64z: number = 0x10;
+    public static readonly enum = 0x00;
+    public static readonly sInt8 = 0x01;
+    public static readonly uInt8 = 0x02;
+    public static readonly sInt16 = 0x03;
+    public static readonly uInt16 = 0x04;
+    public static readonly sInt32 = 0x05;
+    public static readonly uInt32 = 0x06;
+    public static readonly string = 0x07;
+    public static readonly float32 = 0x08;
+    public static readonly float64 = 0x09;
+    public static readonly uInt8z = 0x0A;
+    public static readonly uInt16z = 0x0B;
+    public static readonly uInt32z = 0x0C;
+    public static readonly byte = 0x0D;
+    public static readonly sInt64 = 0x0E;
+    public static readonly uInt64 = 0x0F;
+    public static readonly uInt64z = 0x10;
 
     // And this with the type defn to get the index
-    public static readonly baseTypeNumMask: number = 0x1F;
+    public static readonly baseTypeNumMask = 0x1F;
 }
